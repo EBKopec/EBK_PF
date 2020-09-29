e630979ae8a167a93aa19f95725cdbb2937e2a09
EBKopec <evertonkopec@hotmail.com>
2020-09-29 18:13:34 -0300

    prd

--- EBK_PF/src/components/services/api.js
+++ EBK_PF/src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'http://10.85.24.19:3001/api',
     xsrfCookieName: 'XSRF-TOKEN',
     xsrfHeaderName: 'X-XSRF-TOKEN',
 });
--- EBK_PF/src/components/services/user.service.js
+++ EBK_PF/src/components/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:3001/api/test';
+const API_URL = 'http://10.85.24.19:3001/api/test';
 
 class UserService {
   getPublicContent() {
